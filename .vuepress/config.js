7f595a63be3818ea77a4b180fc505dade03e5f86
hbothra <hemant.bothra@oracle.com>
2019-11-08 14:08:51 +0530

    Updating config file to generate out put to different loaction rather then default, and adding the same(o/p location) to git ignore

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   title: 'Hemant\'s World',
-  description: 'Playground of Hemant,collection of many articles and blogs'
+  description: 'Playground of Hemant,collection of many articles and blogs',
+  dest: 'dist'
 }
